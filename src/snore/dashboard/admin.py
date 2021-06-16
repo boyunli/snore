49dc33aaff7fbfcede19fc17169c3cd189734f8e
lily <515550681@qq.com>
2021-06-17 07:35:59 +0800

    delete add watermark

--- src/snore/dashboard/admin.py
+++ src/snore/dashboard/admin.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 from django.utils.html import format_html
 
 from snore.settings import MEDIA_ROOT
-from snore.utils import add_watermark
+# from snore.utils import add_watermark
 from .models import Category, Article, Tag, Link, SiteSettings
 from .views import settings
 
@@ -77,7 +77,7 @@ class ArticleAdmin(admin.ModelAdmin):
                             img = img.convert('RGB')
                         img.thumbnail((img.width/1.5, img.height/1.5), Img.ANTIALIAS)
                         img.save(image_path, format='JPEG', optimize=True, quality=70)
-                    add_watermark(image_path, image_path)
+                    # add_watermark(image_path, image_path)
                 else:
                     # 不进行重复 crop
                     continue
